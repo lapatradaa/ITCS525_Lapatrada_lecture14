af55c61ae698974f0bbe593c32ea6b72ea425ae2
lapatradaa <106292765+lapatradaa@users.noreply.github.com>
2025-11-15 14:51:29 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+
+from fastapi import FastAPI
+from sqlmodel import create_engine, text
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI + Postgresql running!"}
+
+
+    
+@app.get("/test-db")
+def test_db():
+    with engine.connect() as conn:
+        # change this line:
+        # result = conn.execute(text("SELECT 1"))
+
+        # to:
+        result = conn.execute(text("SELECT 1+1"))
+
+        return {"db_result": result.one()[0]}
+
